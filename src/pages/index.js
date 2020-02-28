432abc5bd58977d95aa35b3916c670494ddc16b3
paliyaana <polina.vauhkonen@gmail.com>
2020-02-28 20:25:51 +0200

    Favicon and background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,52 @@
 import React from "react"
+import { graphql, StaticQuery } from 'gatsby'
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+import BackgroundImage from 'gatsby-background-image'
+
+const BackgroundSection = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "background2.png" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={`white`}
+        >
+          <div style={{ color: `lightcoral` }}>
+          <br/>
+          <h2>polina.business</h2>
+          </div>
+          <h2>welcome to my website</h2>
+          <h2>HELSINKI, FINLAND</h2>
+        </BackgroundImage>
+      )
+    }}
+  />
+)
+
+const StyledBackgroundSection = styled(BackgroundSection)`
+  width: 100%;
+  background-position: top center;
+  background-repeat: repeat-y;
+  background-size: cover;
+`
+
+export default StyledBackgroundSection
+
+
+//export default () => <div>Hello world!</div>
