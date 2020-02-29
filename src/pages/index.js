b3213f0b44f2ff4a567641807a9fb5c924781dd9
paliyaana <polina.vauhkonen@gmail.com>
2020-02-29 18:14:44 +0200

    Stopped using image-plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,57 +2,25 @@ import React from "react"
 import { graphql, StaticQuery } from 'gatsby'
 import { Link } from "gatsby"
 import styled from 'styled-components'
-import yourSVG from '../images/shapes.svg'
+import background_svg from '../images/background.svg'
+import shapes_svg from '../images/shapes.svg'
 import BackgroundImage from 'gatsby-background-image'
 
-const Home = () => <><img src={yourSVG} alt="shapes"/></>
+const Shapes = () => <><img src={shapes_svg} alt="shapes"/></>
+const Background = () => <><img src={background_svg} alt="background" style={{ position: `absolute`, top: `0`}} /></>
 
-const BackgroundSection = ({ className }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        desktop: file(relativePath: { eq: "background.png" }) {
-          childImageSharp {
-            fluid(quality: 100, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      // Set ImageData.
-      const imageData = data.desktop.childImageSharp.fluid
-      return (
-        <BackgroundImage
-          Tag="section"
-          className={className}
-          fluid={imageData}
-          backgroundColor={`white`}
-        >
-          <br/>
-          <p>polina.business</p>
-          <p>welcome to my website</p>
-          <div style={{ fontSize: `3em` }}>
-          <Link to="/contact/">contact</Link>
-          </div>
-          <Home>
-          </Home>
-        </BackgroundImage>
-      )
-    }}
-  />
+export default () => (
+  <div id="parent" class="background">
+   <Background></Background>
+    <span id= "child" class="text">
+     <p>polina.business</p>
+     <p>welcome to my website</p>
+     <div style={{ fontSize: `3em` }}>
+     <Link to="/contact/">contact</Link>
+     </div>
+    </span>
+    <span id= "shape" class="shapes">
+    <Shapes></Shapes>
+    </span>
+  </div>
 )
-
-const StyledBackgroundSection = styled(BackgroundSection)`
-  width: 100%;
-  height: 100%;
-  background-position: left;
-  background-repeat: repeat-y;
-  background-size: cover;
-`
-
-export default StyledBackgroundSection
-
-
-//export default () => <div>Hello world!</div>
