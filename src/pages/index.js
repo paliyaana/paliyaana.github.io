549c996ed1ca930fc7080c51cf68433ac728fb70
paliyaana <polina.vauhkonen@gmail.com>
2020-03-01 13:15:42 +0200

    Crazy layout changes with animation and rainbows

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,16 @@ import React from "react"
 import { graphql, StaticQuery } from 'gatsby'
 import { Link } from "gatsby"
 import styled from 'styled-components'
-import background_svg from '../images/background.svg'
-import shapes_svg from '../images/shapes.svg'
+import BackgroundSvg from '../images/background.svg'
+import ShapesSvg from '../images/shapes.svg'
 import BackgroundImage from 'gatsby-background-image'
 
-const Shapes = () => <><img src={shapes_svg} alt="shapes"/></>
-const Background = () => <><img src={background_svg} alt="background" style={{ position: `absolute`, top: `0`}} /></>
+const Shapes = () => <><ShapesSvg /></>
+const Background = () => <><BackgroundSvg style={{ position: `absolute`, top: `0`}} /></>
 
 export default () => (
   <div id="parent" class="background">
-   <Background></Background>
+   <Background />
     <span id= "child" class="text">
      <p>polina.business</p>
      <p>welcome to my website</p>
@@ -20,7 +20,7 @@ export default () => (
      </div>
     </span>
     <span id= "shape" class="shapes">
-    <Shapes></Shapes>
+      <Shapes />
     </span>
   </div>
 )
