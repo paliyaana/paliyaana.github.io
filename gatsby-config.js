4081ac59f10b8f1c22bc429ea2ec6b6c38ec5740
paliyaana <polina.vauhkonen@gmail.com>
2020-09-08 22:06:21 +0300

    Sadness

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GA_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        anonymize: false,
       },
     },
     {
