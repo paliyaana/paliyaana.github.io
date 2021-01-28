1aaf7cd934c3c9c2c4a0c8b449963b16176a4313
paliyaana <polina.vauhkonen@gmail.com>
2021-01-28 21:57:28 +0200

    Panun syntymäpäivät

---
+++ src/pages/panu.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { graphql, StaticQuery } from 'gatsby'
+
+export default () => (
+  <div id="panu" class="panu" style = {{ height: 2000, marginTop: 0, border: 0, paddingTop: 0 }} >
+  <p>Onneksi olkoon Panu!</p>
+  </div>
+)
