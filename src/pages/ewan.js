c36b828520ffeb0f5a0351cb70a1afbb558c8cdb
paliyaana <polina.vauhkonen@gmail.com>
2021-03-20 13:33:11 +0200

    Ewan2

--- src/pages/ewan.js
+++ src/pages/ewan.js
@@ -4,13 +4,17 @@ import WeedSvg from '../images/weed.svg'
 const Weed = () => <><WeedSvg /></>
 
 export default () => (
-  <div id="container">
-    <span className="text" style = {{ marginLeft: 5 }} >
-      <div style = {{ paddingTop: 5 }} className="what">
-        <p style = {{ paddingTop: 5, marginTop: 0 }}>Sori Ewan vielä eilisestä. Hävettää vieläkin.</p>
+  <div id="container" style = {{ marginTop: 0 }}>
+  <span className="weed" >
+    <Weed />
+  </span>
+    <span className="what" style = {{ marginLeft: 5 }} >
+      <div style = {{ paddingTop: 0 }} className="ewan">
+        <p style = {{ paddingTop: 0, marginTop: 0, marginLeft: 75 }}>Sori Ewan vielä eilisestä</p>
+        <p style = {{ paddingTop: 0, marginTop: 0, marginLeft: 80 }}>ala-arvosesta heitosta.</p>
       </div>
     </span>
-    <span className="weed">
+    <span className="weed" width="100px" height="100%" >
       <Weed />
     </span>
   </div>
